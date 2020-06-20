de4304ac76cd1f2dcdc7c9670309ddc4a4007cb7
Marcelo Martins Dev <marcelomartinsdev@gmail.com>
2020-06-19 22:12:28 -0300

    Updates

---
+++ precache-manifest.a9caeb4138f9c91e2174be7fe57da74c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8c352cced6ef877208832e6503b059d1",
+    "url": "/my-portfolio/index.html"
+  },
+  {
+    "revision": "d399bb45c0e1a112f1a1",
+    "url": "/my-portfolio/static/css/2.e4346227.chunk.css"
+  },
+  {
+    "revision": "27aa23de8f3f79f67403",
+    "url": "/my-portfolio/static/css/main.106e51e4.chunk.css"
+  },
+  {
+    "revision": "d399bb45c0e1a112f1a1",
+    "url": "/my-portfolio/static/js/2.e8a44b12.chunk.js"
+  },
+  {
+    "revision": "e078d673834ef0c088fe44bab9fcc203",
+    "url": "/my-portfolio/static/js/2.e8a44b12.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "27aa23de8f3f79f67403",
+    "url": "/my-portfolio/static/js/main.3213f916.chunk.js"
+  },
+  {
+    "revision": "dcfe90d6f458b2af458e",
+    "url": "/my-portfolio/static/js/runtime-main.8a6a9f30.js"
+  }
+]);
